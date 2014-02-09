d886f5e3dc07043154d5fe0a6fc77d84fc2e4153
Pedro Rogério <pinceladasdaweb@hotmail.com>
2014-02-09 13:38:17 -0200

    Added CSS Banner

--- Gruntfile.js
+++ Gruntfile.js
@@ -11,12 +11,23 @@ module.exports = function (grunt) {
             },
         },
         cssmin: {
-            options: {
-                keepSpecialComments: 1
-            },
-            minify: {
-                src: ['src/style.css', 'src/prettify.css'],
-                dest: 'style.css'
+            with_banner: {
+                options: {
+                    banner: '/* \n' +
+                        'Theme Name: pw \n' +
+                        'Theme URI: http://www.pinceladasdaweb.com.br/blog\n' +
+                        'Description: Tema do blog Pinceladas da Web\n' +
+                        'Author: Comunidade PW \n' +
+                        'Author URI: https://github.com/comunidadepw \n' +
+                        'Version: 0.0.3 \n' +
+                        'Tags: light, two-columns, left-sidebar, fluid-layout, sticky-post \n' +
+                        'License: GNU General Public License v2 or later \n' +
+                        'License URI: http://www.gnu.org/licenses/gpl-2.0.html \n' +
+                        '*/'
+                },
+                files: {
+                    'style.css': ['src/style.css', 'src/prettify.css']
+                }
             }
         }
     });
