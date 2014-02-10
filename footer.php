23df3e89366a597c7344ab6e3bfafa91af6f037a
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2014-02-10 14:10:14 -0200

    Added Google Analytics code

--- footer.php
+++ footer.php
@@ -19,5 +19,15 @@
 <?php wp_footer(); ?>
 
 <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/javascript/all.min.js"></script>
+<script type="text/javascript">
+    var _gaq = _gaq || [];
+    _gaq.push(['_setAccount', '']);
+    _gaq.push(['_trackPageview', document.location.pathname + document.location.search + document.location.hash]);
+    (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    })();
+</script>
 </body>
 </html>
