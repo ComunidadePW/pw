3deebda1ac032960a99df308ecf3c8c6c553dc14
Pedro Rogerio <pedro.lemes@locaweb.com.br>
2014-02-21 16:06:30 -0300

    Added link to home on is_search and 404 pages

--- header.php
+++ header.php
@@ -99,7 +99,7 @@
         </div><!-- #wrap-header-menu -->
 
     </nav><!-- #header-nav -->
-    <?php if (is_category() || is_tag() || is_month() || is_paged() || is_page() || is_single()): ?>
+    <?php if (is_category() || is_tag() || is_month() || is_paged() || is_page() || is_single() || is_search() || is_404()): ?>
         <a href="<?php echo home_url(); ?>" id="logo" class="column-quarter content" rel="index">
             <img src="<?php echo get_template_directory_uri(); ?>/images/logo.png" alt="<?php bloginfo('name'); ?>" title="<?php bloginfo('name'); ?>" />
         </a>
